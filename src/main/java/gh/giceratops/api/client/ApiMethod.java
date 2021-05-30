2f117aed6921a4a089d3763603460fa82c6842ce
gi <igiceratops@gmail.com>
2021-05-30 23:07:04 +0200

    More flexible JSON parsing (dates/missing parameters);

--- src/main/java/gh/giceratops/api/client/ApiMethod.java
+++ src/main/java/gh/giceratops/api/client/ApiMethod.java
@@ -2,8 +2,13 @@ package gh.giceratops.api.client;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 import java.text.SimpleDateFormat;
 
@@ -17,11 +22,16 @@ public enum ApiMethod {
     private final ObjectMapper mapper;
 
     ApiMethod(final ObjectMapper mapper) {
-        this.mapper = mapper
+        this.mapper = JsonMapper.builder() // or different mapper for other format
+                .addModule(new ParameterNamesModule())
+                .addModule(new Jdk8Module())
+                .addModule(new JavaTimeModule())
+                .build()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE)
                 .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
-                .enable(SerializationFeature.INDENT_OUTPUT)
-                .setDateFormat(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss"));
+                .enable(SerializationFeature.INDENT_OUTPUT);
+                //.setDateFormat(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss"));
     }
 
     public ObjectMapper mapper() {
